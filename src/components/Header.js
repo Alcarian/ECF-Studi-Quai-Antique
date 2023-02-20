a667320ed5ac0fd136ab8f28750dc779cfc20e16
Alcarian <floa2@msn.com>
2023-02-20 14:43:39 +0100

    modif navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,14 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <div className="header">
-      <div>
-        <Logo />
-      </div>
       <nav>
+        <Logo />
         <HamburgerMenu />
       </nav>
+      <div className="header-text">
+        <h1>Le Quai Antique</h1>
+        <h6>La Savoie dans votre assiette !</h6>
+      </div>
     </div>
   );
 };
