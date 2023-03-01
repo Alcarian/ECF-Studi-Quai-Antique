1952977c69d9d0742c4c30a448f3ab07405454e0
Alcarian <floa2@msn.com>
2023-03-01 15:33:33 +0100

    modif page norfound2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CardMenu from "./pages/CardMenu";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/ECF-Studi-Quai-Antique/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/CardMenu" element={<CardMenu />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
